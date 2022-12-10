8ed95511cff59b3735580c55ebf0de372019f343
WandersonGomes <dersom100@gmail.com>
2022-12-10 17:58:26 -0300

    Atualizacao do Exercicio01.c para sua versao 1.2 e adicionamento do .gitignore

--- capitulo-03/exercicio01.c
+++ capitulo-03/exercicio01.c
@@ -1,5 +1,5 @@
 /*
-    Autor: João Pedro Araújo Arruda 
+    Autor: Joao Pedro Araujo Arruda 
     Semestre: 1º Semestre
     Email: jpedroaraujo2004@gmail.com
     
@@ -7,21 +7,20 @@
         Exercicio 01 do Capitulo 3
         
         Enunciado: Faça um algoritmo que leia um número inteiro diferente de zero e 
-        diga se este número é positivo ou negativo
-        
-        
-        */
+        diga se este numero e positivo ou negativo        
+*/
 #include <stdio.h>
 
-int main()
-{
+int main() {
     int num;
-    scanf("%d\n", &num);
+    
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &num);
 
-    if(num > 0){    
-    printf("o número é positivo\n");
-    }else if(num < 0){
-        printf("o número é negativo\n");
+    if (num > 0) {    
+        printf("O numero e positivo.\n");
+    } else if (num < 0) {
+        printf("O numero e negativo.\n");
     }
 
     return 0;
