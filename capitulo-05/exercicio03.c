36d9b6a58ded8608954f8007c50f00c65f719cdb
FranciscoJonathan <115195857+FranciscoJonathan@users.noreply.github.com>
2022-12-09 23:22:43 -0300

    Create exercicio03.c

---
+++ capitulo-05/exercicio03.c
@@ -0,0 +1,11 @@
+/*
+    Autor: Francisco Jonathan Rodrigues Fenandes
+    Semestre: 2 semestre
+    Email: fernandes.francisco07@aluno.ifce.edu.br
+    Descricao: 
+        Exercicio 03 do Capitulo 5
+        Enunciado: Faca um algoritmo que calcule a somo dos N primeiros numeros primos, sendo N definido pelo usuario no 
+        modulo principal. O algoritmo ainda devera ter os madulos Soma_Primos e Primos, sendo que o primeiro sera responsavel
+        pela soma dos numeros que forem primos e o segundo sera responsavel por verificar se o numero em quetão eh primo.
+        
+*/
