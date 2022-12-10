03532d9feb6500bab9f15eaa6fdc209ca914171f
FranciscoJonathan <115195857+FranciscoJonathan@users.noreply.github.com>
2022-12-09 21:42:01 -0300

    Update exercicio02.c

--- capitulo-05/exercicio02.c
+++ capitulo-05/exercicio02.c
@@ -1,1 +1,15 @@
+/*
+    Autor: Francisco Jonathan Rodrigues Fenandes
+    Semestre: 2 semestre
+    Email: fernandes.francisco07@aluno.ifce.edu.br
+    Descricao: 
+        Exercicio 01 do Capitulo 5
+        Enunciado: Faca um algoritmo que receba o tamanho dos tres lados de um triangulo e faca as seguintes atividades: retorne 
+        se os tres lados realmente formam um triangulo e que tipo de triangulo eh esse 
+        
+*/
+#include<stdio.h>
 
+int main(){
+  
+}
