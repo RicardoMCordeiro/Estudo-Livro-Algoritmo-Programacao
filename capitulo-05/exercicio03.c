e84bfb76958f3beeb1282994fee7a464142cc315
FranciscoJonathan <115195857+FranciscoJonathan@users.noreply.github.com>
2022-12-10 10:25:51 -0300

    Update exercicio03.c

--- capitulo-05/exercicio03.c
+++ capitulo-05/exercicio03.c
@@ -9,3 +9,51 @@
         pela soma dos numeros que forem primos e o segundo sera responsavel por verificar se o numero em quetão eh primo.
         
 */
+#include <stdio.h>
+/*
+    um numero primo eh um numero maior que um, que pertence ao conjunto dos numeros natural e eh divisivel apenas por um e por
+    ele mesmo. Sendo que a divisao nao pode resultar em restos ou o resultado ser um numero que fuja do conjunto dos naturais.
+*/
+int Soma_Primos(int num);
+int Primo(int num);
+
+int main(void) {
+  int num, somaPrimos;
+  printf("Informe o numero para verificar se eh primo:\n");
+  scanf("%i", &num);
+
+  if(Primo(num) == 2){
+    printf("O numero inserido eh primo!\n");
+  }else{
+    printf("O numero inserido nao eh primo!\n");
+  }
+  somaPrimos = Soma_Primos(num);
+  printf("A soma dos numeros primos de 1 ate %i eh: %i\n", num, somaPrimos);
+  return 0;
+}
+
+int Primo(int num){
+  int qtd = 0;
+  for(int i = 1; i <= num; i++){
+    num / i;
+    if(num % i == 0)
+      qtd++;
+  }
+  return qtd;
+}
+
+int Soma_Primos(int num){
+  int qtd = 0, somaPrimos = 0; 
+  for(int i = 1; i <= num; i++){
+    qtd = 0;
+    for(int j = 1; j <= i; j++){
+      i / j;
+      if(i % j == 0)
+      qtd++;
+    }
+    if(qtd == 2){
+      somaPrimos += i;
+    }
+  }
+  return somaPrimos;
+}
