5fcea097b6353bd2560ffd289e48a44b59f6c8cb
FranciscoJonathan <115195857+FranciscoJonathan@users.noreply.github.com>
2022-12-09 21:35:47 -0300

    Update exercicio01.c

--- capitulo-05/exercicio01.c
+++ capitulo-05/exercicio01.c
@@ -1,17 +1,60 @@
 /*
-    Autor: 
-    Semestre: 
-    Email: 
+    Autor: Francisco Jonathan Rodrigues Fenandes
+    Semestre: 2 semestre
+    Email: fernandes.francisco07@aluno.ifce.edu.br
 
     Descricao: 
         Exercicio 01 do Capitulo 5
 
-        Enunciado:
+        Enunciado: Faca um algoritmo que receba o tamanho dos tres lados de um triangulo e faca as seguintes atividades: retorne 
+        se os tres lados realmente formam um triangulo e que tipo de triangulo eh esse 
         
 */
 #include <stdio.h>
 
 int main() {
 
+    void existenciaTriangulo(float segmento_AB, float segmento_CD, float segmento_EF);
+    void tipoTiangulo(float segmento_AB, float segmento_CD, float segmento_EF);
+/*
+    Segundo a condicao de existencia de um triangulo: tres segmentos de reta so formarao um triangulo, se a soma de dois de
+    dois de seus lados for maior que o terceiro lado.
+    Ex. seguimentos AB = 16, CD = 20, EF = 30
+    AB + CD = 36 > EF
+    CD + EF = 50 > AB
+    AB + EF = 46 > CD
+*/
+    float segmento_AB, segmento_CD, segmento_EF;
+    
+    printf("Informe o valor do primeiro lado do triangulo:");
+    scanf("%f", &segmento_AB);
+    printf("Informe o valor do segundo lado do triangulo:");
+    scanf("%f", &segmento_CD);
+    printf("Informe o valor do terceiro lado do triangulo:");
+    scanf("%f", &segmento_EF);
+    
+    existenciaTriangulo(segmento_AB, segmento_CD, segmento_EF);
+    tipoTiangulo(segmento_AB, segmento_CD, segmento_EF);
+
     return 0;
 }
+
+    void existenciaTriangulo(float segmento_AB, float segmento_CD, float segmento_EF){
+        if(segmento_AB + seguimento_CD > seguimento_EF && seguimento_CD + seguimento_EF > seguimento_AB && seguimento_AB + seguimento_EF > CD){
+            printf("O triangulo possui a condicao de existencia!");
+        }else{
+            printf("O triangulo nao possui a condicao de existencia!");
+        }
+    }
+    void tipoTiangulo(float segmento_AB, float segmento_CD, float segmento_EF){
+        if(segmento_AB + seguimento_CD > seguimento_EF && seguimento_CD + seguimento_EF > seguimento_AB && seguimento_AB + seguimento_EF > CD){
+            if(segmento_AB == seguimento_CD && seguimento_AB == seguimento_EF)
+            printf("Triangulo equilatero.");
+            if(segmento_AB == seguimento_CD && seguimento_AB != seguimento_EF)
+            printf("Triangulo isosceles.");
+            if(segmento_AB != seguimento_CD && seguimento_AB != seguimento_EF)
+            printf("Triangulo escaleno.");
+        }
+    }
+        
+        
