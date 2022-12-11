5d9e2f02e493296e89b8297fc9ed03ffce7c6576
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2022-12-11 15:44:31 -0300

    exercicio03.c versão 1.0

--- capitulo-02/exercicio03.c
+++ capitulo-02/exercicio03.c
@@ -14,5 +14,23 @@
 
 int main() {
 
+    double numero_1, numero_2, numero_3, numero_4, media;
+
+    printf("Digite a sua 1 Nota: ");
+    scanf("%lf", &numero_1);
+
+    printf("Digite a sua 2 Nota: ");
+    scanf("%lf", &numero_2);
+
+    printf("Digite a sua 3 Nota: ");
+    scanf("%lf", &numero_3);
+
+    printf("Digite a sua 4 Nota: ");
+    scanf("%lf", &numero_4);
+    
+    media = (numero_1 + numero_2 + numero_3 + numero_4) / 4;
+
+    printf("A sua Media e: %.2lf", media);
+    
     return 0;
 }
