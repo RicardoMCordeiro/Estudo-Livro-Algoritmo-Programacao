0ca564bd5ec29f7419af435bc907985e42ce5d40
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2022-12-11 15:53:36 -0300

    exercicio04.c versão 1.0

--- capitulo-02/exercicio04.c
+++ capitulo-02/exercicio04.c
@@ -14,5 +14,14 @@
 
 int main() {
 
+    double graus_celsius, graus_fahrenheint;
+
+    printf("Digite a Temperatura em Graus Celsius: ");
+    scanf("%lf", &graus_celsius);
+
+    graus_fahrenheint = (1.8 * graus_celsius) + 32;
+
+    printf("A temperatura %.2lf Graus Celsius em Fahrenheit, e igual a %.2lf!", graus_celsius, graus_fahrenheint);
+
     return 0;
 }
