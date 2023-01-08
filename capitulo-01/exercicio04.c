a481007c8d8dece88b73b6907fab93ed8844bd4d
WandersonGomes <dersom100@gmail.com>
2023-01-08 08:49:49 -0300

    Exercicio 04 - Versao 1.1

--- capitulo-01/exercicio04.c
+++ capitulo-01/exercicio04.c
@@ -4,7 +4,7 @@
     Email: wanderson.gomes.costa05@aluno.ifce.edu.br
 
     Descricao: 
-        Exercicio 03 do Capitulo 1
+        Exercicio 04 do Capitulo 1
 
         Enunciado:
         Defina o que e uma linguagem de programacao de alto nivel e uma linguagem
---
+++ capitulo-01/exercicio05.c
@@ -0,0 +1,19 @@
+/*
+    Autor: Wanderson Gomes da Costa
+    Semestre: 6
+    Email: wanderson.gomes.costa05@aluno.ifce.edu.br
+
+    Descricao: 
+        Exercicio 03 do Capitulo 1
+
+        Enunciado:
+        Defina o que e uma linguagem de programacao de alto nivel e uma linguagem
+        de programacao de baixo nivel.
+*/
+#include <stdio.h>
+
+int main() {
+    
+
+    return 0;
+}
