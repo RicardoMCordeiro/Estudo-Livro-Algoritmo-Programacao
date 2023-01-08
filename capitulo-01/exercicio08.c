b6713c276b3be3732b16172e1aaec08e6c70e796
WandersonGomes <dersom100@gmail.com>
2023-01-08 09:12:53 -0300

    Exercicio 08 - Versao 1.0

---
+++ capitulo-01/exercicio08.c
@@ -0,0 +1,59 @@
+/*
+    Autor: Wanderson Gomes da Costa
+    Semestre: 6
+    Email: wanderson.gomes.costa05@aluno.ifce.edu.br
+
+    Descricao: 
+        Exercicio 08 do Capitulo 1
+
+        Enunciado:
+        Quais as vantagens e desvantagens da utilizacao de fluxograma
+        e de pseudocodigo na construcao de algoritmos?
+*/
+#include <stdio.h>
+
+int main() {
+    printf(
+    "Fluxogramas e pseudocódigos são duas técnicas muito utilizadas na \
+     construção de algoritmos, pois elas permitem representar de forma \
+     visual e clara as etapas e decisões envolvidas no processamento de dados.");
+
+    printf(
+    "Algumas vantagens da utilização de fluxogramas e pseudocódigos na \
+    construção de algoritmos são:");
+
+    printf(
+    "Facilidade de visualização: Fluxogramas e pseudocódigos permitem representar \
+    de forma gráfica e intuitiva as etapas e decisões envolvidas em um algoritmo, \
+    o que pode ser muito útil para entender o funcionamento de um algoritmo e para \
+    identificar possíveis problemas ou otimizações.");
+
+    printf(
+    "Independência de linguagem: Fluxogramas e pseudocódigos são independentes de \
+    qualquer linguagem de programação específica, o que significa que eles podem \
+    ser utilizados para representar algoritmos que serão implementados em qualquer linguagem.");
+
+    printf(
+    "Facilidade de modificação: Fluxogramas e pseudocódigos são muito úteis para fazer \
+    alterações em um algoritmo já existente, pois eles permitem visualizar de forma clara as \
+    etapas e decisões envolvidas, facilitando a identificação de pontos que precisam ser modificados.");
+
+    printf(
+    "Algumas desvantagens da utilização de fluxogramas e pseudocódigos na construção de algoritmos são:");
+
+    printf(
+    "Necessidade de transposição: Após a construção de um algoritmo em forma de fluxograma ou pseudocódigo, \
+    é preciso transpor esse algoritmo para uma linguagem de programação específica para que ele possa ser \
+    executado por um computador. Isso pode ser um trabalho demorado e sujeito a erros.");
+
+    printf(
+    "Dificuldade de depuração: Fluxogramas e pseudocódigos não possuem a mesma precisão e detalhamento de \
+    uma linguagem de programação, o que pode tornar mais difícil a depuração de erros em um algoritmo escrito \
+    nessas técnicas.");
+
+    printf(
+    "Falta de expressividade: Fluxogramas e pseudocódigos possuem um conjunto limitado de símbolos e \
+    palavras-chave, o que pode tornar difícil expressar certas ideias ou construções de forma precisa e concisa.");
+
+    return 0;
+}
