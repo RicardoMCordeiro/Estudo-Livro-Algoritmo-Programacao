6074e51c246184b60b9f6350a45ae6118e847d53
WandersonGomes <dersom100@gmail.com>
2023-01-08 19:27:26 -0300

    Exercicio 03 - Versao 1.2

--- capitulo-04/exercicio03.c
+++ capitulo-04/exercicio03.c
@@ -7,38 +7,68 @@
         Exercicio 03 do Capitulo 4
 
         Enunciado:
-            Faca um algoritmo que calcule a divisao
-            inteira de dois numeros, por exemplo, 10
-            div 3 = 3, utilizando o operador de adicao
-            ou subtracao.
+        Faca um algoritmo que calcule a divisao inteira de dois numeros, por exemplo, 
+        10 div 3 = 3, utilizando o operador de adicao ou subtracao. Depois, faca o teste
+        de mesa para garantir que o exercicio esta correto.
+
+        Para auxiliar na resolucao deste exercicio, observe que a operacao de divisao segue
+        o raciocinio similar ao da multiplicacao, porem de forma inversa. Ou seja, o numero
+        de vezes que se subtraindo o dividendo (operando1) pelo divisor (operando2) chegar
+        a 0 e o quociente procurado.
+
+        Vejamos o exemplo: 12 div 3.
+
+        Operando1: 12
+
+        Operando2: 3
+
+        Quociente: 4
+
+        Se calcularmos 12 - 3 - 3 - 3 - 3 = 0 (12 subtraido 4 vezes (quociente) por 3 e igual a 0).
+
+        Caso o quociente nao seja exato, e preciso verificar que a proxima subtracao tera um resultado
+        negativo:
+
+        13 div 3 -> 13 - 3 - 3 - 3 - 3 = 1.
+
+        Subtraindo-se mais uma vez, teremos -2. Portanto, o resultado procurado e 4.
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 
 int main() {
-    int dividendo, divisor, acumulador_divisao = 0, aux;
+    //dados
+    int operando1,
+        operando2,
+        somador,
+        quociente = 0;
     
-    printf("Digite o dividendo: ");
-    scanf("%d", &dividendo);
-    aux = dividendo;
+    //leitura
+    printf("Informe o operando1: ");
+    scanf("%d", &operando1);
 
     do {
-        if(divisor <= 0)
-            printf("ERRO: Divisor causa indefinicao\n");
+        printf("Informe o operando2: ");
+        scanf("%d", &operando2);
+        //error
+        if (operando2 == 0)
+            puts("ERROR: o operando 2 e invalido! Tente novamente.");
+    } while (operando2 == 0);
 
-        printf("Digite o divisor: ");
-        scanf("%d", &divisor);
-        
-    }while(divisor <= 0);
+    //processamento
+    somador = ( (operando1 > 0 && operando2 < 0) ||
+                (operando1 < 0 && operando2 > 0) ) ? -1 : 1;
 
-    while(aux >= divisor) {
-        acumulador_divisao += 1;
-        aux -= divisor;
+    operando1 = (operando1 > 0) ? operando1 : -operando1;
+    operando2 = (operando2 > 0) ? operando2 : -operando2;
+
+    while (operando1 >= operando2) {
+        quociente += somador;
+        operando1 -= operando2;
     }
-    
-    printf("Divisao inteira entre %d e %d = %d\n", dividendo, divisor, acumulador_divisao);
-    
-    system("pause");
+
+    printf("Quociente: %d\n", quociente);
+
     return 0;
 }
