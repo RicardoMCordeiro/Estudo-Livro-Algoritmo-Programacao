433c7beaeca916dff3c73fcfab9e7093970ad528
WandersonGomes <dersom100@gmail.com>
2023-01-08 09:27:27 -0300

    Exercicio 01 - Versao 1.0

--- capitulo-02/exercicio01.c
+++ capitulo-02/exercicio01.c
@@ -13,6 +13,18 @@
 #include <stdio.h>
 
 int main() {
+    printf(
+    "O papel do programador é responsável por criar o programa e implementar \
+    as funcionalidades nele. Isso inclui a escrita do código fonte, a compilação \
+    e o teste do programa. O programador também pode ser responsável por fazer \
+    alterações e manutenção no programa, incluindo correção de erros e adição de novas \
+    funcionalidades.");
+
+    printf(
+    "O papel do usuário é utilizar o programa para realizar tarefas específicas, \
+    bem como fornecer os dados de entrada para o programa. O usuário também pode ser \
+    responsável por analisar os resultados gerados pelo programa e, caso sejam necessárias, \
+    fazer alterações nas configurações ou nos dados de entrada para obter resultados diferentes.");
 
     return 0;
 }
