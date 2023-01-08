aba0c7104d145dc93339af5526621d623f0b1385
WandersonGomes <dersom100@gmail.com>
2023-01-08 08:34:41 -0300

    Exercicio 02 - Versao 1.0

---
+++ capitulo-01/exercicio02.c
@@ -0,0 +1,30 @@
+/*
+    Autor: Wanderson Gomes da Costa
+    Semestre: 6
+    Email: wanderson.gomes.costa05@aluno.ifce.edu.br
+
+    Descricao: 
+        Exercicio 02 do Capitulo 1
+
+        Enunciado:
+        Diferencie um algoritmo de um programa.        
+*/
+#include <stdio.h>
+
+int main() {
+    printf(
+        "Um algoritmo é uma sequência de passos lógicos"
+        " que são seguidos para resolver um problema ou realizar"
+        " uma tarefa. Em outras palavras, um algoritmo é uma receita "
+        "para resolver um problema.");
+    printf(
+        "Por outro lado, um programa é uma implementação de um algoritmo "
+        "em uma linguagem de programação específica. Em outras palavras, "
+        "um programa é um conjunto de instruções escritas em uma linguagem "
+        "de programação que, quando executadas por um computador, realizam "
+        "uma tarefa ou resolvem um problema.");
+    printf(
+        "Portanto, podemos dizer que um algoritmo é a ideia por trás de um "
+        "programa, enquanto o programa é a forma concreta de implementar esse algoritmo.");
+    return 0;
+}
