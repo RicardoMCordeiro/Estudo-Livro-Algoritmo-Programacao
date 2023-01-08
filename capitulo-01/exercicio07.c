9a358617c03468539a56f78a986b5bace1a2d067
WandersonGomes <dersom100@gmail.com>
2023-01-08 09:05:12 -0300

    Exercicio 07 - Versao 1.0

---
+++ capitulo-01/exercicio07.c
@@ -0,0 +1,39 @@
+/*
+    Autor: Wanderson Gomes da Costa
+    Semestre: 6
+    Email: wanderson.gomes.costa05@aluno.ifce.edu.br
+
+    Descricao: 
+        Exercicio 07 do Capitulo 1
+
+        Enunciado:
+        Por que a linguagem natural nao e adequada para a construcao de
+        algoritmos para computador?
+*/
+#include <stdio.h>
+
+int main() {
+    printf(
+    "A linguagem natural é a forma como os seres humanos \
+     se comunicam entre si, utilizando palavras e frases \
+     para expressar ideias e pensamentos. Ela é muito adequada \
+     para a comunicação entre humanos, mas não é tão adequada para \
+     a construção de algoritmos para computadores.");
+
+    printf(
+    "Isso se deve ao fato de que a linguagem natural é muito vaga \
+    e ambígua, o que pode levar a interpretações diferentes de uma \
+    mesma frase por diferentes pessoas. Além disso, ela é muito \
+    subjetiva e depende do contexto em que é utilizada, o que pode \
+    tornar difícil a sua interpretação por parte de um computador.");
+
+    printf(
+    "Por outro lado, as linguagens de programação são muito mais \
+    precisas e específicas, e possuem regras bem definidas para a \
+    sua sintaxe e semântica. Isso permite que os algoritmos escritos \
+    em uma linguagem de programação sejam facilmente interpretados e \
+    executados pelos computadores, sem ambiguidades ou interpretações \
+    incorretas.");
+
+    return 0;
+}
