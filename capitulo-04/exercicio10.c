2f49f8f995a58e36c85f5a4685d908939e15387d
WandersonGomes <dersom100@gmail.com>
2023-01-08 20:17:21 -0300

    Exercicio 10 - Versao 1.2

--- capitulo-04/exercicio10.c
+++ capitulo-04/exercicio10.c
@@ -7,39 +7,55 @@
         Exercicio 10 do Capitulo 4
 
         Enunciado:
-            Faca um algoritmo que encontre o n-esimo
-            termo da serie de Fibonacci.
-*/
+        Faca um algoritmo que encontre o n-esimo termo da serie de Fibonacci. A serie
+        de Fibonacci e dada por:
+        
+        fib(n) = fib(n - 1) + fib(n - 2) para n > 1;
 
+        Para n = 0 e n = 1, o valor e dado por definicao: fib(0) = 0 e fib(1) = 1.
+
+        Exemplos:
+        
+        fib(0) = 0
+        fib(1) = 1
+        fib(2) = fib(1) + fib(0) = 1 + 0 = 1
+        fib(3) = fib(2) + fib(1) = 1 + 1 = 2
+        fib(4) = fib(3) + fib(2) = 2 + 1 = 3
+        fib(5) = fib(4) + fib(3) = 3 + 2 = 5
+        fib(6) = fib(5) + fib(4) = 5 + 3 = 8
+*/
 #include <stdio.h>
-#include <stdlib.h>
 
 int main() {
-    int valor_fatorial, a = 0, b = 1, c, aux;
+    //dados
+    int termo,
+        fib,
+        n1 = 0,
+        n2 = 1;
     
-    printf("Digite um numero inteiro: ");
-    scanf("%d", &valor_fatorial);
+    //leitura
+    do {
+        printf("Informe o termo que deseja saber da serie de fibonacci: ");
+        scanf("%d", &termo);
+        //error
+        if (termo < 0)
+            puts("ERROR: termo invalido! Tente novamente.");
+    } while (termo < 0);
 
-    aux = valor_fatorial;
 
-    while(aux - 2 >= 0) {
-        // c (novo termo) é a soma dos anteriores (a+b)
-        c = a + b;
-        /*
-            a   b   c(a+b)
-            0   1   1
-            <----
-            a   b   c(a+b novos)
-            1   1   2
-        */
-        a = b;
-        b = c;
-        // diminui o número de repetições até chegar ao caso base F(2) = 1
-        aux -= 1;
+    //processamento
+    if (termo == 0) fib = n1;
+    else if (termo == 1) fib = n2;
+    else {
+        for (int i = 2; i <= termo; i++) {
+            fib = n2 + n1;
+            n1 = n2;
+            n2 = fib;
+        }
     }
 
-    printf("Fibonacci(%d) = %d\n", valor_fatorial, c);
+    //saida
+    printf("fib(%d) = %d\n", termo, fib);
 
-    system("pause");
     return 0;
 }
