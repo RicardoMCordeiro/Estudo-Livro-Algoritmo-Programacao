044603cb8c1f1d64bc5cc746f71459b673509731
WandersonGomes <dersom100@gmail.com>
2023-01-08 08:46:29 -0300

    Exercicio 04 - Versao 1.0

---
+++ capitulo-01/exercicio04.c
@@ -0,0 +1,34 @@
+/*
+    Autor: Wanderson Gomes da Costa
+    Semestre: 6
+    Email: wanderson.gomes.costa05@aluno.ifce.edu.br
+
+    Descricao: 
+        Exercicio 03 do Capitulo 1
+
+        Enunciado:
+        Defina o que e uma linguagem de programacao de alto nivel e uma linguagem
+        de programacao de baixo nivel.
+*/
+#include <stdio.h>
+
+int main() {
+    printf(
+    "As linguagens de programação de alto nível são aquelas que\
+     estão mais próximas da linguagem natural humana e são mais\
+     fáceis de serem lidas e escritas pelos humanos. Elas geralmente\
+     possuem sintaxe mais clara e simplificada e são mais abstraídas\
+     em relação às operações de baixo nível que ocorrem em um computador.\
+     Exemplos de linguagens de programação de alto nível incluem Python, Java, C# e C++.");
+
+    printf(
+    "Por outro lado, as linguagens de programação de baixo nível\
+     são mais próximas da linguagem de máquina, ou seja, o código\
+     fonte é mais próximo do código binário que é executado pelo\
+     processador. Elas são mais difíceis de serem lidas e escritas\
+     pelos humanos e requerem mais conhecimento sobre os detalhes \
+     de funcionamento de um computador. Exemplos de linguagens de \
+     programação de baixo nível incluem Assembly e código binário.");
+
+    return 0;
+}
