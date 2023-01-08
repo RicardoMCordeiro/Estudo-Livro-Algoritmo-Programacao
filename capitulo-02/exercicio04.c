6c37fb62f62cb6c2c138fab6499695b1f467b6c4
WandersonGomes <dersom100@gmail.com>
2023-01-08 09:19:56 -0300

    Exercicio 04 - Versao 1.1

--- capitulo-02/exercicio04.c
+++ capitulo-02/exercicio04.c
@@ -21,7 +21,7 @@ int main() {
 
     graus_fahrenheint = (1.8 * graus_celsius) + 32;
 
-    printf("A temperatura %.2lf Graus Celsius em Fahrenheit, e igual a %.2lf!", graus_celsius, graus_fahrenheint);
+    printf("A temperatura %.2lf Graus Celsius em Fahrenheit, e igual a %.2lf!\n", graus_celsius, graus_fahrenheint);
 
     return 0;
 }
