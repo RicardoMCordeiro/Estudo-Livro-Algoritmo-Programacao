ee6162e8e6389252049780414ea2325824a7f1e3
WandersonGomes <dersom100@gmail.com>
2023-01-08 08:30:53 -0300

    Exercicio 01 - Versao 1.0

--- capitulo-01/exercicio01.c
+++ capitulo-01/exercicio01.c
@@ -1,17 +1,19 @@
 /*
-    Autor: 
-    Semestre: 
-    Email: 
+    Autor: Wanderson Gomes da Costa
+    Semestre: 6
+    Email: wanderson.gomes.costa05@aluno.ifce.edu.br
 
     Descricao: 
         Exercicio 01 do Capitulo 1
 
         Enunciado:
+        Defina o que e um algoritmo.
         
 */
 #include <stdio.h>
 
 int main() {
-
+    printf("Um algoritmo é uma sequência de passos lógicos que são seguidos"
+           "para resolver um problema ou realizar uma tarefa.");
     return 0;
 }
